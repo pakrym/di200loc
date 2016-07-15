c601e3fb335cc5777ee2c3bcc493061023dd52f9
Pavel Krymets <pavel@krymets.com>
2016-07-15 08:57:20 -0700

    Setup

---
+++ src/SimpleDI/ServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleDI
+{
+    public class ServiceProvider : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/SimpleDI.Tests/SimpleDISpecificationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Specification;
+
+namespace SimpleDI.Tests
+{
+    public class SimpleDISpecificationTests : DependencyInjectionSpecificationTests
+    {
+        protected override IServiceProvider CreateServiceProvider(IServiceCollection collection) =>
+            new ServiceProvider();
+    }
+}
